bcbdb8b400fd2c87453288c2f9040c01166ad576
Fazorrazor <Fazorrazor@users.noreply.github.com>
2026-06-29 19:01:16 +0000

    perf: implement db indexing and dynamic component imports

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,6 +1,8 @@
 import { Hero } from '../../components/home/Hero';
-import { DivisionGrid } from '../../components/home/DivisionGrid';
-import { TrustBadges } from '../../components/home/TrustBadges';
+import dynamic from 'next/dynamic';
+
+const TrustBadges = dynamic(() => import('../../components/home/TrustBadges').then((mod) => mod.TrustBadges));
+const DivisionGrid = dynamic(() => import('../../components/home/DivisionGrid').then((mod) => mod.DivisionGrid));
 
 export const revalidate = 3600;
 export default function HomePage() {
